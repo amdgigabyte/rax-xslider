8b9f0c923a5342ff271189efcf253aaa6a9f5ad4
yacheng.sz <yacheng.sz@taobao.com>
2019-01-16 17:56:20 +0800

    feat: add test

---
+++ src/__tests__/index.js
@@ -0,0 +1,9 @@
+import {createElement, Component} from 'rax';
+import renderer from 'rax-test-renderer';
+import XSlider from '../';
+
+describe('XSlider', () => {
+  it('test typeof XSlider', () => {
+    expect(typeof XSlider).toEqual('function');
+  });
+});
